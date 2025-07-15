e55519d8e959b719e585dccdc79b0d6936edd9d3
JeisonCerpa <cerpajeison8@gmail.com>
2025-07-15 11:40:10 -0500

    Añadir descripciones a los estados de seguimiento de mantenimiento y mejorar la vista de seguimiento con nuevas columnas para Semana, Año y Estado.

--- Modules/GestionMantenimientos/Models/Enums/EstadoSeguimientoMantenimiento.cs
+++ Modules/GestionMantenimientos/Models/Enums/EstadoSeguimientoMantenimiento.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel;
+
 namespace GestLog.Modules.GestionMantenimientos.Models.Enums
 {
     public enum EstadoSeguimientoMantenimiento
     {
+        [Description("Pendiente")]
         Pendiente = 0, // No realizado y la semana aún no ha terminado
+        [Description("Realizado en tiempo")]
         RealizadoEnTiempo = 1, // Realizado dentro de la semana correspondiente
+        [Description("Realizado fuera de tiempo")]
         RealizadoFueraDeTiempo = 2, // Realizado después de la semana programada
+        [Description("Atrasado")]
         Atrasado = 3 // No realizado y la semana ya terminó
     }
 }
